055d45ba93baac8666cce4bd487a69951a22641b
SloppyJack <312883327@qq.com>
2020-06-05 23:18:34 +0800

    记账界面设计中

--- src/view/recordAccount/record-create.vue
+++ src/view/recordAccount/record-create.vue
@@ -6,32 +6,37 @@
                 <div slot="header"><span>支出与收入</span></div>
                 <el-tabs v-model="activeName" @tab-click="handleClick">
                     <el-tab-pane label="支出" name="first">
-                        <el-dropdown @command="handleCommand">
+                        <el-row>
+                        <el-col :lg="16" :md="20" :sm="24" :xs="24">
+                            <el-dropdown @command="handleCommand">
                             <span class="el-dropdown-link">
                             下拉菜单<i class="el-icon-arrow-down el-icon--right"></i>
                             </span>
-                            <el-dropdown-menu slot="dropdown">
-                                <el-dropdown-item command="黄金糕">黄金糕</el-dropdown-item>
-                                <el-dropdown-item command="狮子头">狮子头</el-dropdown-item>
-                                <el-dropdown-item command="螺蛳粉">螺蛳粉</el-dropdown-item>
-                                <el-dropdown-item disabled >双皮奶</el-dropdown-item>
-                                <el-dropdown-item divided command="蚵仔煎">蚵仔煎</el-dropdown-item>
-                            </el-dropdown-menu>
-                        </el-dropdown>
+                                <el-dropdown-menu slot="dropdown">
+                                    <el-dropdown-item command="黄金糕">黄金糕</el-dropdown-item>
+                                    <el-dropdown-item command="狮子头">狮子头</el-dropdown-item>
+                                    <el-dropdown-item command="螺蛳粉">螺蛳粉</el-dropdown-item>
+                                    <el-dropdown-item disabled >双皮奶</el-dropdown-item>
+                                    <el-dropdown-item divided command="蚵仔煎">蚵仔煎</el-dropdown-item>
+                                </el-dropdown-menu>
+                            </el-dropdown>
+                        </el-col>
+                        </el-row>
                     </el-tab-pane>
                     <el-tab-pane label="收入" name="second">
-                        <el-row class="block-col-2">
-                            <el-col :span="12">
-                                <span class="demonstration">分类</span>
-                                <el-dropdown @command="handleCommand">
-                                    <el-button type="primary">请选择<i class="el-icon-arrow-down el-icon--right"></i> </el-button>
-                                    <el-dropdown-menu slot="dropdown">
-                                        <el-dropdown-item command="工资">工资</el-dropdown-item>
-                                        <el-dropdown-item command="兼职">兼职</el-dropdown-item>
-                                        <el-dropdown-item command="理财">理财</el-dropdown-item>
-                                        <el-dropdown-item command="其它">其它</el-dropdown-item>
-                                    </el-dropdown-menu>
-                                </el-dropdown>
+                        <el-row class="block-col-2" :gutter="20">
+                            <el-col :span="4">
+                                <div class="demonstration">分类</div>
+                            </el-col>
+                            <el-col :span="4">
+                                <el-select  size="medium"  v-model="value" placeholder="请选择">
+                                    <el-option
+                                            v-for="item in options"
+                                            :key="item.value"
+                                            :label="item.label"
+                                            :value="item.value">
+                                    </el-option>
+                                </el-select>
                             </el-col>
                         </el-row>
                         <el-collapse class="test" style="color:red;">
@@ -50,15 +55,17 @@
 export default {
   data() {
     return {
-      activeName: 'second'
+      options: [
+        '工资',
+        '兼职',
+        '理财',
+        '其他'
+      ]
     }
   },
   methods: {
-    handleClick(tab, event) {
-      console.log(tab, event)
-    },
     handleCommand(command) {
-      this.$message('点击了 ' + command)
+      this.$message(`点击了 ${command}`)
     }
   }
 
