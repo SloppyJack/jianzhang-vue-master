5564bbf8dbf0243baff03717c7ad88d82fbeb7b4
SloppyJack <312883327@qq.com>
2020-06-15 21:17:28 +0800

    实现记账编辑

--- src/view/record/record-list.vue
+++ src/view/record/record-list.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- 列表页面 -->
-    <div class="container" v-if="!showEdit">
+    <div class="container">
       <div class="header">
         <div class="header-left"><p class="title">记账明细</p></div>
         <div class="header-right">
@@ -14,61 +14,38 @@
       <!-- 表格 -->
       <el-table
         :data="tableData"
-        :operate="operate"
         @row-click="rowClick"
         @expand-change="expandChange"
+        :expand-row-keys="expends"
         height="600"
         v-loading="loading"
       >
         <el-table-column type="expand">
-          <el-collapse-transition>
-            <div v-show="expendFlag">
-              <el-form label-position="left">
-                <el-form-item label="商品名称">
-                  <span>1</span>
-                </el-form-item>
-                <el-form-item label="所属店铺">
-                  <span>1</span>
-                </el-form-item>
-                <el-form-item label="商品 ID">
-                  <span>1</span>
-                </el-form-item>
-                <el-form-item label="店铺 ID">
-                  <span>1</span>
-                </el-form-item>
-              </el-form>
-            </div>
-          </el-collapse-transition>
-              <!--<template slot-scope="props">
-                <el-collapse-transition>
-                <div v-show="expendFlag">
-                  <el-form label-position="left">
-                    <el-form-item label="商品名称">
-                      <span>{{ props.row.name }}</span>
+              <template slot-scope="props">
+                <div>
+                  <el-form label-width="100px">
+                    <el-form-item label="花费类别">
+                      <el-input placeholder="备注内容" size="medium" suffix-icon="el-icon-edit"
+                                v-model="props.row.spend_category.name" :disabled="true" style="width: 50%;">
+                      </el-input>
                     </el-form-item>
-                    <el-form-item label="所属店铺">
-                      <span>{{ props.row.shop }}</span>
+                    <el-form-item label="金额">
+                      <el-input-number v-model="props.row.amount" size="min" :min="0"
+                                       :precision="2" :step="5" style="width: 50%;"></el-input-number>
                     </el-form-item>
-                    <el-form-item label="商品 ID">
-                      <span>{{ props.row.id }}</span>
+                    <el-form-item label="发生时间">
+                      <el-input placeholder="在何时消费的~" size="medium" suffix-icon="el-icon-edit"
+                                v-model="props.row.occur_time" :disabled="true" style="width: 50%;">
+                      </el-input>
                     </el-form-item>
-                    <el-form-item label="店铺 ID">
-                      <span>{{ props.row.shopId }}</span>
-                    </el-form-item>
-                    <el-form-item label="商品分类">
-                      <span>{{ props.row.category }}</span>
-                    </el-form-item>
-                    <el-form-item label="店铺地址">
-                      <span>{{ props.row.address }}</span>
-                    </el-form-item>
-                    <el-form-item label="商品描述">
-                      <span>{{ props.row.desc }}</span>
+                    <el-form-item label="备注">
+                      <el-input placeholder="在何时消费的~" size="medium" suffix-icon="el-icon-edit"
+                                v-model="props.row.remarks" style="width: 50%;">
+                      </el-input>
                     </el-form-item>
                   </el-form>
                 </div>
-                </el-collapse-transition>
               </template>
--->
         </el-table-column>
         <el-table-column v-for="(item,index) in tableColumn" :key="index" :label="item.label"
                          :prop="item.prop" :min-width="item.width">
@@ -81,26 +58,20 @@
         </el-table-column>
       </el-table>
     </div>
-
-    <!-- 编辑页面 -->
-    <book-modify v-else @editClose="editClose" :editBookID="editBookID"></book-modify>
   </div>
 </template>
 
 <script>
 import record from '@/model/record'
 import LinSearch from '@/component/base/search/lin-search'
-import BookModify from '../book/book-modify'
-import CollapseTransition from 'element-ui/lib/transitions/collapse-transition';
 
 export default {
   components: {
-    BookModify,
-    LinSearch,
-    CollapseTransition
+    LinSearch
   },
   data() {
     return {
+      loading: false,
       tableColumn: [
         { prop: 'amount', label: '金额', width: '200' },
         { prop: 'spend_category.name', label: '花费类别', width: '200' },
@@ -108,31 +79,18 @@ export default {
         { prop: 'remarks', label: '备注', width: '250' }
       ],
       tableData: [],
-      operate: [],
-      showEdit: false,
-      editBookID: 1,
-      expendFlag: false
+      expends: []
     }
   },
   async created() {
     this.loading = true
     await this.getRecords()
-    this.operate = [
-      { name: '编辑', func: 'handleEdit', type: 'primary' },
-      {
-        name: '删除',
-        func: 'handleDelete',
-        type: 'danger',
-        permission: '删除图书',
-      },
-    ]
     this.loading = false
   },
   methods: {
     async getRecords() {
       try {
         const records = await record.getRecords()
-        console.log(records)
         this.tableData = records
       } catch (error) {
         if (error.code === 10020) {
@@ -142,11 +100,6 @@ export default {
     },
     handleClick(obj) {
       console.log(obj)
-    },
-    handleEdit(val) {
-      console.log('val', val)
-      this.showEdit = true
-      this.editBookID = val.row.id
     },
     handleDelete(val) {
       this.$confirm('此操作将永久删除该图书, 是否继续?', '提示', {
@@ -165,17 +118,17 @@ export default {
       })
     },
     rowClick() {},
-    editClose() {
-      this.showEdit = false
-      this.getBooks()
-    },
     onQueryChange(val) {
       this.$message(`${val}`)
     },
     // eslint-disable-next-line
     expandChange(row,expandedRows) {
       // 当用户对某一行展开或者关闭的时候会触发该事件
-      this.expendFlag = !this.expendFlag
+      console.log(expandedRows)
+      /*expandedRows.forEach((item,index) => {
+        this.recordTypes.push(item)
+      })*/
+      console.log(this.expends)
     }
   },
 }
